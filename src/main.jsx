5220c0070ec2e5bdf938ebbf48f71e5978d666c5
vodcars1-ship-it <vodcars1@gmail.com>
2026-03-08 22:59:50 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VodCarsHomepage from "./VodCarsHomepage";
+import VodCarsHomepage from "./VodCarsHomepage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
