62d29fbc5bed874abd8501196a67bda1ce0c72b3
vodcars1-ship-it <vodcars1@gmail.com>
2026-03-09 00:13:34 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style.css";
 import VodCarsHomepage from "./VodCarsHomepage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
